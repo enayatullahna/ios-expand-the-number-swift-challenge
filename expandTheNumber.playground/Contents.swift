c95aa5a2e5ff3bf694f1de2c824cf0245beda265
Lambda_School_Loaner_188 <lambda_school_loaner_188@LambdaSr188sMBP.fios-router.home>
2019-09-24 19:18:56 -0700

    Completed

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,49 @@
 import Foundation
 
+
+
 func expandTheNumber(_ number: Int) -> [Int] {
     
+    
+    let expandNum = String(describing: number).compactMap { Int(String($0)) }
+    
+    var numb = [Int]()
+
+    if expandNum.count > 4 {
+        
+        numb.append(expandNum[0] * 10000)
+        numb.append(expandNum[1] * 1000)
+        numb.append(expandNum[2] * 100)
+        numb.append(expandNum[3] * 10)
+        numb.append(expandNum[4] * 1 )
+        
+    } else if expandNum.count > 3 {
+
+        numb.append(expandNum[0] * 1000)
+        numb.append(expandNum[1] * 100)
+        numb.append(expandNum[2] * 10)
+        numb.append(expandNum[3] * 1 )
+        
+        
+    } else if expandNum.count > 2 {
+        
+        numb.append(expandNum[0] * 100)
+        numb.append(expandNum[1] * 10)
+        numb.append(expandNum[2] * 1)
+        
+    } else if expandNum.count > 1 {
+        
+        numb.append(expandNum[0] * 10)
+        numb.append(expandNum[1] * 1)
+        
+    } else {
+        
+        numb.append(expandNum[0])
+        
+    }
+
+    return numb
+
 }
 
 expandTheNumber(199)  // [100, 90, 9]
